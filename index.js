99cc7ddaa39e8b818623265676f4ad60f556b006
king <king@kings-MacBook-Air.local>
2023-07-03 19:38:29 +0200

    dropdown menu added with js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('jsak')
+const menu = document.getElementById('viewer')
+const drop = document.getElementById('optione')
+const bod = document.getElementById('bodyy')
+
+function viewIt(){
+    drop.classList.toggle('view')
+    bod.classList.toggle('stop')
+
+}
