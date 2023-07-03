934eece79363cc4c9011d1464a0302bc1b2948d0
king <king@kings-MacBook-Air.local>
2023-07-03 20:03:08 +0200

    closing button added for a drop down

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const menu = document.getElementById('viewer')
 const drop = document.getElementById('optione')
 const bod = document.getElementById('bodyy')
+const openeR =document.getElementById('opener') 
+const closeR = document.getElementById('closer')
 
 function viewIt(){
     drop.classList.toggle('view')
     bod.classList.toggle('stop')
+    openeR.classList.toggle('open')
+    closeR.classList.toggle('fade')
+
 
 }
